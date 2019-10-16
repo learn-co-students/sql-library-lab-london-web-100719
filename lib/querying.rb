7c2e6b74dbfc0c00fd452710569fb4f99fd44c9d
geewey <geewey@gmail.com>
2019-10-16 15:45:51 +0100

    Done.

--- lib/querying.rb
+++ lib/querying.rb
@@ -42,7 +42,8 @@ def select_series_title_with_most_human_characters
   ON character_books.character_id = characters.id
   WHERE characters.species = 'human'
   GROUP BY characters.species
-  HAVING COUNT(characters.species);"
+  ORDER BY COUNT(characters.species); DESC
+  LIMIT 1"
 end
 
 def select_character_names_and_number_of_books_they_are_in
